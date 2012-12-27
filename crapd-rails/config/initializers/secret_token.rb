8d8859619b9fafb76f1ae5295a01ce824b662825
Steve Dierker <steve.dierker@obstkiste.org>
2012-12-27 01:32:27 +0100

    Added initial rails app

---
+++ crapd-rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrapdRails::Application.config.secret_token = '014a40948312a04c8cf286067e56c909fbfece621c37a10c16faadb21d466cf20731aa205578abd6e87b362b91f7809af242d795cb81776340aa7352edac0670'
